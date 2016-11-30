a3db8e8e10b0be944a5f743c7ea6aaeb6f16ea0b
Xavier Lepretre <xavier.lepretre@gmail.com>
2016-11-30 13:50:12 +0000

    Put back deployed MetaCoin, just to get all tests to pass.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.autolink();
-  // deployer.deploy(MetaCoin);
+  deployer.deploy(ConvertLib);
+  deployer.autolink();
+  deployer.deploy(MetaCoin);
   deployer.deploy(Warehouse)
   	.then(function () {
   		return deployer.deploy(AppStore, Warehouse.address);
